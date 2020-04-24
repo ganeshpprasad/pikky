5654fa9459d492a25e4e788bb534ad08f256f516
ganeshpprasad <ganeshpprasad@gmail.com>
2020-04-24 16:54:52 +0530

    start topbar icons

--- src/components/OTPbuttons.js
+++ src/components/OTPbuttons.js
@@ -4,7 +4,7 @@ import {View, TextInput} from 'react-native';
 import {styles} from '../styles/onBoardChat';
 
 const OTPbuttons = () => {
-    const [otp, setOtp] = useState('');
+    const [otp, setOtp] = useState(['', '', '', '']);
     const ref1 = useRef(null);
     const ref2 = useRef(null);
     const ref3 = useRef(null);
@@ -13,9 +13,10 @@ const OTPbuttons = () => {
     const handleKeyPress = (pos, e) => {
         const {key} = e.nativeEvent;
         if (key === 'Backspace') {
-            focusOnPrevText(pos);
-            const newStr = otp.slice(0, otp.length - 1);
-            setOtp(newStr);
+            focusOnPrevText(pos + 1);
+            const newOtp = otp;
+            newOtp[pos] = '';
+            setOtp(newOtp);
             console.log('pre');
         }
     };
@@ -46,49 +47,51 @@ const OTPbuttons = () => {
         ref && ref.current.focus();
     };
 
-    const setOtpInPos = num => {
+    const setOtpInPos = (pos, num) => {
         const charCode = num.charCodeAt(0);
         if (charCode > 47 && charCode < 58) {
             console.log('asdf');
             focusOnNextText();
-            const newArr = otp.concat(num);
+            const newArr = otp;
+            newArr[pos] = num;
             setOtp(newArr);
         }
     };
+    console.log('otp', otp);
 
     return (
         <View style={[styles.otpButtonCon]}>
             <TextInput
                 maxLength={1}
                 style={styles.otpButton}
-                value={otp.charAt(0)}
+                value={otp[0]}
                 autoFocus
-                onKeyPress={e => handleKeyPress(1, e)}
-                onChangeText={setOtpInPos}
+                onKeyPress={e => handleKeyPress(0, e)}
+                onChangeText={text => setOtpInPos(0, text)}
                 ref={ref1}
             />
             <TextInput
                 maxLength={1}
                 style={styles.otpButton}
-                value={otp.charAt(1)}
-                onKeyPress={e => handleKeyPress(2, e)}
-                onChangeText={setOtpInPos}
+                value={otp[1]}
+                onKeyPress={e => handleKeyPress(1, e)}
+                onChangeText={text => setOtpInPos(1, text)}
                 ref={ref2}
             />
             <TextInput
                 maxLength={1}
                 style={styles.otpButton}
-                value={otp.charAt(2)}
-                onKeyPress={e => handleKeyPress(3, e)}
-                onChangeText={setOtpInPos}
+                value={otp[2]}
+                onKeyPress={e => handleKeyPress(2, e)}
+                onChangeText={text => setOtpInPos(2, text)}
                 ref={ref3}
             />
             <TextInput
                 maxLength={1}
                 style={[styles.otpButton, styles.otpButtonLast]}
-                value={otp.charAt(3)}
-                onKeyPress={e => handleKeyPress(4, e)}
-                onChangeText={setOtpInPos}
+                value={otp[3]}
+                onKeyPress={e => handleKeyPress(3, e)}
+                onChangeText={text => setOtpInPos(3, text)}
                 ref={ref4}
             />
         </View>
