59a2c14fc1c42a8c93fc25de8636863963fc0a6d
ganeshpprasad <ganeshpprasad@gmail.com>
2020-04-26 13:22:02 +0530

    remove names and cleanup loginjs

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -2,7 +2,7 @@
  * Landing screen if the user is not logged in.
  */
 
-import React, {useState} from 'react';
+import React from 'react';
 import {
     SafeAreaView,
     KeyboardAvoidingView,
@@ -30,81 +30,73 @@ const {
     topbar,
 } = styles;
 
+const ImageItem = (source, style) => (
+    <Image source={source} style={style} resizeMode="contain" />
+);
+
+const ImageCircle = source => ImageItem(source, pikkySignal);
+
+const ImageItemLogo = () =>
+    ImageItem(require('../assets/onBoarding/logo2x.png'), pikkyImage);
+
+const ImageShad = () =>
+    ImageItem(require('../assets/onBoarding/shad3x.png'), shadowOfPikky);
+
+const ImagesCon = msgNumber => (
+    <View style={msgNumber > 0 ? brandingConHidden : brandingCon}>
+        {ImageCircle(require('../assets/onBoarding/43x.png'))}
+        {ImageCircle(require('../assets/onBoarding/33x.png'))}
+        {ImageCircle(require('../assets/onBoarding/23x.png'))}
+        {ImageCircle(require('../assets/onBoarding/13x.png'))}
+        {ImageItemLogo()}
+        {ImageShad()}
+    </View>
+);
+
+const topbarRender = msgNumber =>
+    msgNumber > 0 ? (
+        <View style={topbar}>
+            {ImageItem(require('../assets/onBoarding/chatPikkyIcon3.png'))}
+            <Image
+                source={require('../assets/onBoarding/question3.png')}
+                resizeMode="contain"
+            />
+        </View>
+    ) : null;
+
+const ChatCon = (msgNumber, msgData, setUserMsgNumber, componentId) => (
+    <KeyboardAvoidingView
+        behavior={Platform.Os === 'ios' ? 'padding' : 'padding'}
+        keyboardVerticalOffset={220}
+        style={msgNumber > 0 ? chatConFullScreen : chatCon}>
+        {topbarRender(msgNumber)}
+        <OnBoardChat
+            msgData={msgData}
+            setmsgNumber={setUserMsgNumber}
+            msgNumber={msgNumber}
+            componentId={componentId}
+            userResponses={userResponses}
+        />
+    </KeyboardAvoidingView>
+);
+
 const Login = props => {
     const {msgData, setUserMsgNumber, msgNumber} = useOnBoardChat(
         onBoardingData,
     );
-    const [loginFullScreen, setLoginFullScreen] = useState(false);
-
-    if (msgNumber > 0) {
-        !loginFullScreen ? setLoginFullScreen(true) : null;
-    }
 
     return (
         <>
             <StatusBar barStyle="light-content" />
             <SafeAreaView>
                 <View style={loginCon}>
-                    <View
-                        style={
-                            loginFullScreen ? brandingConHidden : brandingCon
-                        }>
-                        <Image
-                            source={require('../assets/onBoarding/43x.png')}
-                            style={pikkySignal}
-                            resizeMode="contain"
-                        />
-                        <Image
-                            source={require('../assets/onBoarding/33x.png')}
-                            style={pikkySignal}
-                            resizeMode="contain"
-                        />
-                        <Image
-                            source={require('../assets/onBoarding/23x.png')}
-                            style={pikkySignal}
-                            resizeMode="contain"
-                        />
-                        <Image
-                            source={require('../assets/onBoarding/13x.png')}
-                            style={pikkySignal}
-                            resizeMode="contain"
-                        />
-                        <Image
-                            source={require('../assets/onBoarding/logo2x.png')}
-                            style={pikkyImage}
-                            resizeMode="contain"
-                        />
-                        <Image
-                            source={require('../assets/onBoarding/shad3x.png')}
-                            style={shadowOfPikky}
-                            resizeMode="contain"
-                        />
-                    </View>
-
-                    <KeyboardAvoidingView
-                        behavior={Platform.Os === 'ios' ? 'padding' : 'padding'}
-                        keyboardVerticalOffset={220}
-                        style={loginFullScreen ? chatConFullScreen : chatCon}>
-                        {loginFullScreen ? (
-                            <View style={topbar}>
-                                <Image
-                                    source={require('../assets/onBoarding/chatPikkyIcon3.png')}
-                                    resizeMode="contain"
-                                />
-                                <Image
-                                    source={require('../assets/onBoarding/question3.png')}
-                                    resizeMode="contain"
-                                />
-                            </View>
-                        ) : null}
-                        <OnBoardChat
-                            msgData={msgData}
-                            setmsgNumber={setUserMsgNumber}
-                            msgNumber={msgNumber}
-                            componentId={props.componentId}
-                            userResponses={userResponses}
-                        />
-                    </KeyboardAvoidingView>
+                    {ImagesCon(msgNumber)}
+                    {ChatCon(
+                        msgNumber,
+                        msgData,
+                        setUserMsgNumber,
+                        props.componentId,
+                    )}
                 </View>
             </SafeAreaView>
         </>
