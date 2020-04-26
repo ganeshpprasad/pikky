1e16b1dc82033e9e6910b8d485c43b386bf702ff
ganeshpprasad <ganeshpprasad@gmail.com>
2020-04-26 13:01:44 +0530

    useraccount done

--- src/screens/UserAccount.js
+++ src/screens/UserAccount.js
@@ -6,48 +6,47 @@ import {styles as msgStyles} from '../styles/chatMsgListStyle';
 
 const {userMsgCon, pikkyMsgCon, botMsgText, msgCon} = msgStyles;
 
+const LogoSection = () => (
+    <View style={styles.logoCon}>
+        <Image
+            source={require('../assets/onBoarding/onboardfinish3.png')}
+            // style={pikkySignal}
+            resizeMode="contain"
+        />
+    </View>
+);
+
+const FinalUserMsgs = msg => (
+    <View style={[pikkyMsgCon, msgCon, styles.pikkyMsgYellow]}>
+        <Text style={botMsgText}>{msg}</Text>
+    </View>
+);
+
+const Buttons = (msg, isLeft) => {
+    const leftButtonStyle = isLeft ? styles.leftButton : null;
+    return (
+        <View style={[userMsgCon, msgCon, styles.finishMsgs, leftButtonStyle]}>
+            <Text style={[botMsgText, styles.finishMsgText]}>{msg}</Text>
+        </View>
+    );
+};
+
+const FinalButtons = () => (
+    <View style={styles.finishMsgsCon}>
+        {Buttons('Later Pikky!', true)}
+        {Buttons('Finally!', false)}
+    </View>
+);
+
 const UserAccount = () => {
     return (
         <View style={styles.onboardfinishCon}>
-            <View style={styles.logoCon}>
-                <Image
-                    source={require('../assets/onBoarding/onboardfinish3.png')}
-                    // style={pikkySignal}
-                    resizeMode="contain"
-                />
-            </View>
+            {LogoSection()}
             <View style={styles.onboardMsgsCon}>
-                <View style={[pikkyMsgCon, msgCon, styles.pikkyMsgYellow]}>
-                    <Text style={botMsgText}>
-                        {'Sweet tooth! You are all set.'}
-                    </Text>
-                </View>
-                <View style={[pikkyMsgCon, msgCon, styles.pikkyMsgYellow]}>
-                    <Text style={botMsgText}>{'Now, let’s talk food.'}</Text>
-                </View>
-                <View style={styles.finishMsgsCon}>
-                    <View
-                        style={[
-                            userMsgCon,
-                            msgCon,
-                            styles.finishMsgs,
-                            styles.leftButton,
-                        ]}>
-                        <Text style={[botMsgText, styles.finishMsgText]}>
-                            {'Later Pikky!'}
-                        </Text>
-                    </View>
-                    <View style={[userMsgCon, msgCon, styles.finishMsgs]}>
-                        <Text style={[botMsgText, styles.finishMsgText]}>
-                            {'Finally!'}
-                        </Text>
-                    </View>
-                </View>
-                <View style={[pikkyMsgCon, msgCon, styles.pikkyMsgYellow]}>
-                    <Text style={botMsgText}>
-                        {'P.S. I am always hungry :)'}
-                    </Text>
-                </View>
+                {FinalUserMsgs('Sweet tooth! You are all set.')}
+                {FinalUserMsgs('Now, let’s talk food.')}
+                {FinalButtons()}
+                {FinalUserMsgs('P.S. I am always hungry :)')}
             </View>
         </View>
     );
