9f495f6506be141efd81f67dceaf2cf5577f7f37
ganeshpprasad <ganeshpprasad@gmail.com>
2020-05-28 17:52:29 +0530

    login done

---
+++ src/screens/Login/components/LoginDone.js
@@ -0,0 +1,77 @@
+import React, {PureComponent} from 'react';
+import {Text, Image, ImageBackground, TouchableOpacity} from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
+
+import DONE from '../assets/done.png';
+import LOGO from '../assets/donka.png';
+
+export default class LoginDone extends PureComponent {
+    render() {
+        return (
+            <ImageBackground source={DONE} style={styles.con}>
+                <Image source={LOGO} />
+                <Text style={styles.verified}> Verified </Text>
+                <Text style={styles.text}>
+                    Let us explore your food habits to create a flavourful
+                    profile!
+                </Text>
+                <TouchableOpacity>
+                    <LinearGradient
+                        colors={[
+                            'rgb(255, 230, 72)',
+                            'rgb(255, 125, 99)',
+                            'rgb(255, 115, 102)',
+                        ]}
+                        start={{x: 0, y: 0}}
+                        end={{x: 1, y: 1}}
+                        style={styles.linearGradient}>
+                        <Text style={styles.buttonText}>FOODIE</Text>
+                    </LinearGradient>
+                </TouchableOpacity>
+            </ImageBackground>
+        );
+    }
+}
+
+const styles = {
+    con: {
+        backgroundImage: DONE,
+        flex: 1,
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    verified: {
+        color: 'white',
+        fontFamily: 'OpenSans-Regular',
+        fontWeight: 'bold',
+        fontSize: 24,
+        marginTop: 20,
+    },
+    text: {
+        fontFamily: 'OpenSans-Regular',
+        fontSize: 20,
+        marginTop: 20,
+        paddingRight: 20,
+        paddingLeft: 20,
+        textAlign: 'center',
+    },
+    linearGradient: {
+        marginTop: 20,
+        paddingLeft: 20,
+        paddingRight: 20,
+        borderRadius: 5,
+        borderTopRightRadius: 35,
+        borderBottomLeftRadius: 40,
+        borderTopLeftRadius: 35,
+    },
+    buttonText: {
+        fontSize: 16,
+        fontFamily: 'OpenSans-Regular',
+        fontWeight: 'bold',
+        textAlign: 'center',
+        margin: 10,
+        color: '#ffffff',
+        backgroundColor: 'transparent',
+    },
+};
--- src/screens/Login/index.js
+++ src/screens/Login/index.js
@@ -1,13 +1,71 @@
 import React, {PureComponent} from 'react';
 import {Text, View, TouchableOpacity, Image, TextInput} from 'react-native';
 
+import LoginDone from './components/LoginDone';
+
 import BACK from './assets/back.png';
 import HELP from './assets/help.png';
 import LOGO from './assets/logo.png';
 import NEXT from './assets/next.png';
+import NEXTA from './assets/nexta.png';
+import ERROR from './assets/i.png';
 
 export default class Login extends PureComponent {
+    constructor(props) {
+        super(props);
+        this.state = {
+            phone: '',
+            activateNext: false,
+            isError: false,
+            errorMsg: '',
+            otp: false,
+            otpNum: '',
+            loginDone: false,
+        };
+    }
+
+    openOtpField = () => {
+        if (this.state.otp) {
+            this.setState({loginDone: true});
+        }
+        if (
+            !this.state.phone.match(
+                /^(?:(?:\+|0{0,2})91(\s*[\ -]\s*)?|[0]?)?[789]\d{9}|(\d[ -]?){10}\d$/,
+            )
+        ) {
+            this.setState({isError: true, errorMsg: 'Enter A valid Number'});
+            return;
+        } else {
+            this.setState({otp: true, activateNext: false});
+        }
+    };
+
+    setPhoneNum = t => {
+        this.setState({activateNext: false});
+        if (t.length === 10) {
+            this.setState({activateNext: true});
+        }
+        this.setState({
+            isError: false,
+        });
+        this.setState({phone: t});
+    };
+
+    setOtp = t => {
+        this.setState({activateNext: false});
+        if (t.length === 4) {
+            this.setState({activateNext: true});
+        }
+        this.setState({
+            isError: false,
+        });
+        this.setState({otpNum: t});
+    };
+
     render() {
+        if (this.state.loginDone) {
+            return <LoginDone />;
+        }
         return (
             <View style={styles.loginCon}>
                 <View>
@@ -23,21 +81,62 @@ export default class Login extends PureComponent {
                         <Image style={styles.logo} source={LOGO} />
                     </View>
                     <View style={styles.mainTextCon}>
-                        <Text style={styles.mainText}>
-                            What is your mobile phone number?
-                        </Text>
+                        {this.state.otp ? (
+                            <Text style={styles.mainText}>
+                                Enter the OTP sent to your mobile.
+                            </Text>
+                        ) : (
+                            <Text style={styles.mainText}>
+                                What is your mobile phone number?
+                            </Text>
+                        )}
                     </View>
-                    <View style={styles.textInputCon}>
-                        <Text style={styles.textInputP}>+91</Text>
-                        <TextInput
-                            keyboardType="numeric"
-                            style={styles.textInput}
-                        />
-                    </View>
+                    {!this.state.otp ? (
+                        <View style={styles.textInputCon}>
+                            <Text style={styles.textInputP}>+91</Text>
+                            <TextInput
+                                autoFocus={true}
+                                maxLength={10}
+                                value={this.state.phone}
+                                onChangeText={this.setPhoneNum}
+                                keyboardType="numeric"
+                                style={styles.textInput}
+                            />
+                        </View>
+                    ) : (
+                        <View style={styles.textInputCon}>
+                            <TextInput
+                                autoFocus={true}
+                                maxLength={6}
+                                value={this.state.otpNum}
+                                onChangeText={this.setOtp}
+                                keyboardType="numeric"
+                                style={styles.textInput}
+                            />
+                        </View>
+                    )}
+                    {this.state.isError && (
+                        <>
+                            <Text style={styles.errorMsg}>
+                                <Image
+                                    style={styles.errorIcon}
+                                    source={ERROR}
+                                />
+                                {'  '}
+                                {this.state.errorMsg}
+                            </Text>
+                        </>
+                    )}
                 </View>
                 <View style={styles.bottomBtnCon}>
-                    <TouchableOpacity style={styles.bottomBtn}>
-                        <Image source={NEXT} />
+                    <TouchableOpacity
+                        onPress={this.openOtpField}
+                        style={styles.bottomBtn}>
+                        {this.state.activateNext ? (
+                            <Image source={NEXTA} />
+                        ) : (
+                            <Image source={NEXT} />
+                        )}
                     </TouchableOpacity>
                 </View>
             </View>
@@ -46,6 +145,14 @@ export default class Login extends PureComponent {
 }
 
 const styles = {
+    errorMsg: {
+        color: 'rgb(255, 115, 102)',
+        textAlign: 'center',
+        marginTop: 10,
+    },
+    errorIcon: {
+        paddingRight: 5,
+    },
     loginCon: {
         display: 'flex',
         flexDirection: 'column',
